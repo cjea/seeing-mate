4905c537ceb72f23d512416ff33c7d67ff60d239
CJ <cjeapel@gmail.com>
2025-06-15 22:52:58 -0700

    post images

--- server.py
+++ server.py
@@ -3,15 +3,16 @@ import time
 def info(msg):
   print(f'{time.ctime()} {msg}')
 
-# info("Importing packages.")
-# from pathlib import Path
-# from fastai.vision.all import load_learner
+info("Importing packages.")
+from pathlib import Path
+from fastai.vision.all import L, load_learner, PILImage, Resize
 
-import IPython
+# import IPython
 from fastapi import FastAPI, Request, File, UploadFile
 from fastapi.responses import FileResponse
 
 api = FastAPI()
+learner = load_learner("model.pkl")
 
 @api.get('/ok')
 def ok_200():
@@ -29,5 +30,13 @@ def style_css():
 @api.post('/predict')
 def predict_img(request: Request, image: UploadFile=File(...)):
   print(f'predict_img')
+  normal = Resize(224)
+  pil = normal(PILImage.create(image.file))
   # IPython.embed()
-  return 200
+  results = learner.predict(pil)
+
+  return {
+      "cat": results[0]
+    , "vocab": list(learner.dls.vocab)
+    , "confidences": results[2].tolist()
+  }
